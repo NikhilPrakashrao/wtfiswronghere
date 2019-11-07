c1d31abdcb51abe051d0d0f79a5af91078c0cd50
Nikhil <nikhilprakashrao@gmail.com>
2019-11-07 16:33:08 +0530

    Tenth challenge completed

--- 10_challenge/10_challenge.py
+++ 10_challenge/10_challenge.py
@@ -8,8 +8,19 @@ For all integers between 1 and 99 (include both):
     # print buzz for multiples of 5 
     # print fizzbuzz for multiples of 3 and 5"
 """
-import fizzbuzz
+from fizzbuzz import fizzbuzz
+
+
 
 #----START OF SCRIPT
 if __name__=='__main__':
     fizzbuzz(100)
+
+
+"""a. I didn't get any clue.
+
+b. i googled the error"'module' object is not callable" and got the answer.
+
+c.I learned that while calling the module we should specify the name. 
+If we just write"import fizzbuzz" from where it should import so if we write "import fizzbuzz from fizzbuzz" 
+it'll come to know that there is a class named as fizzbuzz.(This explaination is according to my understandings)"""
